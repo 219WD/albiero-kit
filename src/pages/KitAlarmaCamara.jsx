f6e08c54aea9a59d06d762c470d142253e7b4e5e
Juan Canepa <macgunmanmc@gmail.com>
2026-03-11 14:18:21 -0300

    correccion navkit

--- src/pages/KitAlarmaCamara.jsx
+++ src/pages/KitAlarmaCamara.jsx
@@ -6,12 +6,12 @@ import RespaldoAlbiero from '../components/KitAlarmaCamara/RespaldoAlbiero.jsx';
 import FAQ from '../components/KitAlarmaCamara/FAQ.jsx'
 import FinalCTA from '../components/KitAlarmaCamara/FinalCTA.jsx'
 import Footer from '../components/Footer.jsx'
-import HeroNav from '../components/KitAlarmaCamara/HeroNav.jsx'
+import HeroNavKit from '../components/KitAlarmaCamara/HeroNavKit.jsx'
 
 export default function KitAlarmaCamara() {
     return (
         <div className='homeScreen-container'>
-            <HeroNav />
+            <HeroNavKit />
             <Herosecurity />
             <SecurityMarquee />
             <ExpertiseAreas />
