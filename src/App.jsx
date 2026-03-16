f1c12ad74672dc31f79f71c62452b4901c73d170
Juan Canepa <macgunmanmc@gmail.com>
2026-03-16 18:59:11 -0300

    instale clarity npm

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Clarity from '@microsoft/clarity';
 import HomeScreen from './pages/HomeScreen.jsx';
 import KitAlarmaCamara from './pages/KitAlarmaCamara.jsx';
 import SeguimientoVehicular from './pages/SeguimientoVehicular.jsx';
-import Alarmas from './pages/Alarmas.jsx'
-import Camaras from './pages/Camaras.jsx'
+import Alarmas from './pages/Alarmas.jsx';
+import Camaras from './pages/Camaras.jsx';
 import Analiticas from './pages/Analiticas/Analiticas';
 import DeteccionIncendios from './pages/DeteccionIncendios.jsx';
 
 function App() {
+    useEffect(() => {
+        Clarity.init('vt5t07r3a8');
+    }, []);
+
     return (
         <Router>
             <Routes>
